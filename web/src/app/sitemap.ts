305b2b70aa66c4ac31cf089bcb8455ee91a5512f
Saptarshi Roy <saptarshiroy39@gmail.com>
2026-09-28 17:40:15 +0530

    Update sitemap.ts

--- web/src/app/sitemap.ts
+++ web/src/app/sitemap.ts
@@ -10,5 +10,59 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1.0,
     },
+    {
+      url: "https://ranno.hirishi.in/introduction",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/installation",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/gn",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/ex",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/xp",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/sv",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/cf",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/author",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://ranno.hirishi.in/resources",
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
